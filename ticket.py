fed9a62e370974b2c9899dcaff8239371aefef9c
Punnatat Rojekjit <pondpun123@gmail.com>
2023-03-16 17:32:48 +0700

    <feat> ticket

---
+++ ticket.py
@@ -0,0 +1,30 @@
+class Ticket:
+    def __init__(self, flight, aircraftseat, passenger, seatbook, extraservice, baggage, meal, specialbaggage) :
+        self._flight = flight
+        self.seatbook = seatbook
+        self._aircraft_seat = aircraftseat
+        self._passenger = passenger
+        self.extraservice = extraservice
+        self.baggage = baggage
+        self.meal = meal
+        self.specialbaggage = specialbaggage
+
+    def create_extraservice():
+        pass
+
+    def create_baggage():
+        pass
+
+    def create_meal():
+        pass
+
+    def create_special_bagage():
+        pass
+
+    def create_special_assistance():
+        pass
+
+    def get_ticket():
+        pass
+    
+    
