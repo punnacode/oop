1a646dfbe83d38d6f5c9c9964393628ba10e047b
Punnatat Rojekjit <pondpun123@gmail.com>
2023-03-16 17:08:52 +0700

    <feat> booking

---
+++ booking.py
@@ -0,0 +1,35 @@
+class Booking:
+    def __init__(self, id, num_of_passenger, seat_booked, payment_status, package_type, ticket_list):
+        self._id = id
+        self._num_of_passenger = num_of_passenger
+        self._seat_booked = seat_booked
+        self._payment_status = payment_status
+        self._package_type = package_type
+        self._list_of_ticket = ticket_list
+
+    def create_ticket():
+        pass
+
+    def get_book_seat():
+        pass
+
+    def add_seat_ticket():
+        pass
+
+    def create_seatbook():
+        pass
+
+    def add_addon():
+        pass
+
+    def sum_price():
+        pass
+
+    def create_payment():
+        pass
+
+    def update_booking_status():
+        pass
+
+    def get_ticket():
+        pass
