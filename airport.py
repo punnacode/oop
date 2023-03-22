4e68cabb47df40f7ff7142653e26114089ddae01
o0XDuser0o <secretdata3351@gmail.com>
2023-03-22 17:58:35 +0700

    <fix2> search flight

--- airport.py
+++ airport.py
@@ -30,16 +30,19 @@ class Airport:
 
     def add_flight_instance(self,flight_instance):
         if isinstance(flight_instance,FlightInstance):
-            self._flight_list.append(flight_instance)
+            self._flight_instance_list.append(flight_instance)
 
     def get_arrive_airport_list(self):
         _arrive_airport_list = []
         for flight in self._flight_list:
             if flight._arrive_airport not in _arrive_airport_list:
-                print(flight._arrive_airport)
                 _arrive_airport_list.append(flight._arrive_airport)
         return _arrive_airport_list
 
-    def get_flight_instance_list(self):
-        return self._flight_instance_list
+    def get_date_list(self,arrive_airport):
+        _date_list = []
+        for flight_instance in self._flight_instance_list:
+            if flight_instance._arrive_airport == arrive_airport and flight_instance._date_depart not in _date_list:
+                _date_list.append(flight_instance._date_depart)
+        return _date_list
     
--- main.py
+++ main.py
@@ -11,13 +11,22 @@ dd406 = flight.Flight("DD406",90,False,AirportA,AirportB)
 AirportA.add_flight(dd406)
 dd405_20230518 = flight.FlightInstance("DD405",90,False,AirportA,AirportB,"2023/05/18","18.30","20.00")
 AirportA.add_flight_instance(dd405_20230518)
-dd406_20230518 = flight.FlightInstance("DD405",90,False,AirportA,AirportB,"2023/05/18","20.30","22.00")
-AirportA.add_flight_instance(dd406_20230518)
+dd405_20230519 = flight.FlightInstance("DD406",90,False,AirportA,AirportB,"2023/05/19","20.30","22.00")
+AirportA.add_flight_instance(dd405_20230519)
 
 airport_list = AirportCatalog.get_list_airport()
 for i in airport_list:
     print(i._name)
-depart_airport = airport_list[int(input())]
+depart_airport = airport_list[int(input())-1]
 
 arrive_airport_list = depart_airport.get_arrive_airport_list()
-print(arrive_airport_list)
+for i in arrive_airport_list:
+    print(i._name)
+arrive_airport = arrive_airport_list[int(input())-1]
+
+date_list = depart_airport.get_date_list(arrive_airport)
+for i in date_list:
+    print(i)
+date_depart = date_list[int(input())-1]
+
+
