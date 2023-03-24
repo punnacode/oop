ba02484d0f6140611722b4f4f9eb007a979544f6
n0e0t <pongsapadnet@gmail.com>
2023-03-24 23:02:05 +0700

    <feat>showseat(all)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from airport import Airport,AirportCatalog,Aircraft
+from aircraft import SeatType
+from enum import Enum
 
 airportcatalog = AirportCatalog()
 AirportA = Airport("AirportA")
@@ -11,6 +13,8 @@ AirportA.create_flight("DD406",90,False,AirportA,AirportB)
 AirportA.create_flight_instance("DD405","2023/05/18","18.30","20.00",dm254)
 AirportA.create_flight_instance("DD405","2023/05/19","18.30","20.00",dm254)
 AirportA.create_flight_instance("DD406","2023/05/18","20.30","22.00",dm254)
+dm254.create_seat(1,"A",SeatType.Normal)
+dm254.create_seat(10,"B",SeatType.Premium)
 
 ##Search Flight
 ##  input depart airport
@@ -36,7 +40,13 @@ date_depart = date_list[int(input())-1]
 flight_instance_list = depart_airport.get_flight_instance_list(arrive_airport,date_depart)
 for i in flight_instance_list:
     print(i.name , i.time_depart , i.time_arrive)
+flight = flight_instance_list[int(input())-1]
 
+## show seat
+
+aircraft_seat = flight.aircarft.get_seat()
+for i in aircraft_seat:
+    print(i.seat_row,i.seat_column,i.seat_type)
 
 
 
