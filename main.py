8f81178619c2ab14c38fa48a2187a438534ad09a
n0e0t <pongsapadnet@gmail.com>
2023-03-29 13:19:13 +0700

    <feat>add_booking

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from airport import Airport,AirportCatalog,Aircraft
 from add_on import PackageCatalog,Package
-from aircraft import SeatType
+from aircraft import SeatType,SeatBook
 from enum import Enum
 from booking import Booking
-
+from Passenger import Passenger,PassengerType,TitleType
+from payment import Payment,PaymentStatus,PaymentType
 
 ## Airport instance 
 
@@ -84,11 +85,33 @@ aircraft_seat = flight_instance.aircraft.get_seat()
 for i in aircraft_seat:
     print(i.seat_row,i.seat_column,i.seat_type)
 seat = aircraft_seat[int(input())-1]
-##สมมุติใส่ชื่อ 
-name = input("input your name")
-##สมมุติหลังใส่ชื่อจะสร้าง booking
-BookingA = Booking(1,1,seat,False,seat.seat_type,1,package)
-flight_instance.add_booking(BookingA)
 
+#setup passenger
+PassengerA = Passenger(PassengerType.ADULT.name,TitleType.MR.name,"BOB","OK","25/09/46","Thai","Thailand","65010971","B","25/01/69")
+PassengerB = Passenger(PassengerType.INFANT.name,TitleType.MISS.name,"M","OK","25/12/48","Thai","Thailand","65010999","B","25/02/69")
+PassengerC = Passenger(PassengerType.INFANT.name,TitleType.MSTR.name,"BB","OK","25/12/60","Thai","Thailand","65010009","B","25/02/69")
+
+#test add_parent
+PassengerA.add_parent(PassengerB)
+PassengerA.add_parent(PassengerC)
+
+#show member
+Member_of_parent = PassengerA.parent
+for i,member in enumerate(Member_of_parent):
+    print(f"Member{i+1}:")
+    print(member)
+    
+#setup booking
+id_of_booking = 69
+num_of_passenger = Passenger.num_passenger
+seatbookA = SeatBook(False,2,'A',SeatType.FRONTROW.name)
+seat_booked = seatbookA.seat_booked
+paymentA = Payment(PaymentType.QRCODE.name,1,id_of_booking,num_of_passenger,None,False)
+payment_status = paymentA.payment_status
+
+package_type = package.name
+
+BookingA = PassengerA.create_booking(id_of_booking,num_of_passenger,"0922513540","A@gmail.com",seat_booked,payment_status,package_type)
+flight_instance.add_booking(BookingA)
 
 
