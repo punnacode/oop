7e8e768ccb04307ff5e3915b876ba7e8f27abfe5
Punnatat Rojekjit <pondpun123@gmail.com>
2023-03-29 17:19:45 +0700

    <feat> admin

--- admin.py
+++ admin.py
@@ -1,18 +1,62 @@
 from promotion import PromotionCatalog
+from airport import Airport, AirportCatalog
+from aircraft import Aircraft
+from flight import Flight, FlightInstance
 
 class Admin:
-    def __init__(self, username, password):
+    def __init__(self, username, password, admin_airport):
         self._username = username
         self._password = password
+        self._admin_airport = admin_airport
 
-    def edit_flight_instance():
-        pass
+    def create_flight(self,name,flight_duration,international,depart_airport,arrive_airport):
+    
+        if isinstance(name,str) and isinstance(flight_duration,int) and isinstance(international,bool) and isinstance(depart_airport,Airport) and isinstance(arrive_airport,Airport):
+            self._admin_airport.create_flight(name,flight_duration,international,depart_airport,arrive_airport)
+        else:
+            raise TypeError("Parameter type not correct")
 
-    def create_flight_instance():
-        pass
+    def create_flight_instance(self,flight_name,date_depart,time_arrive,time_depart,aircraft,price):
+        if isinstance(flight_name,str) and isinstance(date_depart,str) and isinstance(time_arrive,str) and isinstance(time_depart,str) and isinstance(aircraft,Aircraft) and isinstance(price,float):
+            for f in self._admin_airport._flight_list:
+                if f.name == flight_name:
+                    flight = f
+                    break
+            self._admin_airport._flight_instance_list.append(FlightInstance(flight.name,flight.flight_duration,flight.international,flight.depart_airport,flight.arrive_airport,date_depart,time_arrive,time_depart,aircraft,price))
+        else:
+            raise TypeError("Parameter type not correct")
+        
+    def get_flight_instance_list(self,arrive_airport,date_depart):
+        flight_instance_list = []
+        for flight_instance in self._flight_instance_list:
+            if flight_instance.arrive_airport == arrive_airport and flight_instance.date_depart == date_depart and flight_instance not in flight_instance_list:
+                flight_instance_list.append(flight_instance)
+        return flight_instance_list
+
+    def edit_flight_instance(self,flight_instance,edit_name,edit_date_depart,edit_time_arrive,edit_time_depart,edit_aircraft,edit_price):
+        flight_instance.flight_name = edit_name
+        flight_instance.date_depart = edit_date_depart
+        flight_instance.time_arrive = edit_time_arrive
+        flight_instance.time_depart = edit_time_depart
+        flight_instance.aircraft = edit_aircraft
+        flight_instance.price = edit_price
+
+    def cancel_flight_instance(self,flight_instance):
+        del self._admin_airport._flight_instance_list[flight_instance]
 
     def add_promotion(self,promotion_code,discount):
         if isinstance(promotion_code,str) and isinstance(discount,int) :
                 PromotionCatalog._promotion_list.append((promotion_code,discount))
         else:
             raise TypeError("Parameter type is not correct")
+        
+class Adminlist:
+    def __init__(self):
+        self._admin_list = []
+    
+    def add_admin(self, admin):
+        if isinstance(admin,Admin):
+            self._admin_list.append(admin)
+    def get_list_admin(self):
+        return self._admin_list
+    
---
+++ testadmin.py
@@ -0,0 +1,61 @@
+from airport import Airport,AirportCatalog,Aircraft
+from add_on import PackageCatalog,Package
+from aircraft import SeatType
+from enum import Enum
+from admin import Admin, Adminlist
+
+## Airport instance 
+airportcatalog = AirportCatalog()
+AirportA = Airport("AirportA")
+airportcatalog.add_airport(AirportA)
+AirportB = Airport("AirportB")
+airportcatalog.add_airport(AirportB)
+
+#Admin instance
+adminlist = Adminlist()
+AdminA = Admin("bob", "wowza567",AirportA)
+adminlist.add_admin(AdminA)
+selected_flight = []
+
+## Aircraft instance
+dm254 = Aircraft("DM254")
+## Flight instance
+AdminA.create_flight("DD405",90,False,AirportA,AirportB,)
+AdminA.create_flight("DD406",90,False,AirportA,AirportB)
+
+## FlightInstance instance
+AdminA.create_flight_instance("DD405","2023-04-01","18.30","20.00",dm254,1000.00)
+AdminA.create_flight_instance("DD405","2023-05-18","18.30","20.00",dm254,1000.00)
+AdminA.create_flight_instance("DD405","2023-05-19","18.30","20.00",dm254,1000.00)
+AdminA.create_flight_instance("DD406","2023-05-18","20.30","22.00",dm254,1000.00)
+
+##Search Flight(Ton)
+##  input depart airport
+airport_list = airportcatalog.get_list_airport()
+for i in airport_list:
+    print(i.name)
+depart_airport = airport_list[int(input())-1]
+##  input arrive airport
+arrive_airport_list = depart_airport.get_arrive_airport_list()
+for i in arrive_airport_list:
+    print(i.name)
+arrive_airport = arrive_airport_list[int(input())-1]
+
+##  input date
+date_list = depart_airport.get_date_list(arrive_airport)
+for i in date_list:
+    print(i)
+date_depart = date_list[int(input())-1]
+
+##Select FlightInstance(Ton)
+##  show FlightInstance
+flight_instance_list = depart_airport.get_flight_instance_list(arrive_airport,date_depart)
+for i in flight_instance_list:
+    print(i.name , i.time_depart , i.time_arrive)
+flight = flight_instance_list[int(input())-1]
+
+flight.date_depart = str(input("new: "))
+print(flight.date_depart)
+print(flight)
+
+x = int(input())
